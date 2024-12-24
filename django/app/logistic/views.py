84592e3fb51b6c766b6ba25799895a86ce7a33ad
Logiegi <les.med@mail.ru>
2024-12-24 04:57:14 +0300

    initial commit

---
+++ django/app/logistic/views.py
@@ -0,0 +1,20 @@
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['products']
